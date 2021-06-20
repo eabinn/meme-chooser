e9f34286e53be82ab513e83c26f4824bc5266dde
inbaeyoonkr <iby2455@gmail.com>
2021-06-21 00:03:08 +0900

    Create Category component

---
+++ src/components/Category.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="category" v-on:click="$emit('select', title)">
+    <div class="content">
+      <img :src="image" />
+    </div>
+    <div class="title">
+      <h3>{{ title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Category',
+  props: ['title', 'image', 'select']
+});
+</script>
+
+<style scoped lang="scss">
+.category {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  & ~ & {
+    margin: 10px;
+  }
+}
+.title {
+  margin-bottom: 0.2rem;
+  h3 {
+    margin: 0;
+    font-weight: bold;
+    color: #000;
+  }
+}
+.content {
+  width: 100px;
+  height: 100px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
