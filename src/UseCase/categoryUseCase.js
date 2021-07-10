e3e45f82500b8707eb9bbbdcbc2de6616eb13b47
inbaeyoonkr <iby2455@gmail.com>
2021-07-10 22:26:03 +0900

    Remove previous useless files

--- src/UseCase/categoryUseCase.js
+++
@@ -1,9 +0,0 @@
-import categoryAPI from "../api/categoryApi";
-
-const categoryUseCase = {
-  getCategoryList: (categories) => {
-    categoryAPI.GET_LIST(categories);
-  },
-};
-
-export default categoryUseCase;
