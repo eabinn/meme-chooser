c80113ec93d77e7ff4b9a56f262efdf9b1a44143
inbaeyoonkr <iby2455@gmail.com>
2021-07-10 22:27:51 +0900

    User vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { createApp } from "vue";
+import { createWebHashHistory, createRouter } from "vue-router";
 import App from "./App.vue";
+import Home from "@/pages/Home.vue";
+import Select from "@/pages/Select.vue";
+import Result from "@/pages/Result.vue";
+
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/select", name: "Select", component: Select },
+  { path: "/result", name: "Result", component: Result, props: (route: any) => ({ query: route.query.category }) },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: routes,
+});
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount("#app");
